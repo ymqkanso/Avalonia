98f2944d47c399ffb2fd1883b64a75543362a887
Steven Kirk <grokys@gmail.com>
2019-09-10 15:54:47 +0200

    Scroll latest output into view.

--- src/Avalonia.Diagnostics/Diagnostics/Views/ConsoleView.xaml.cs
+++ src/Avalonia.Diagnostics/Diagnostics/Views/ConsoleView.xaml.cs
@@ -1,17 +1,24 @@
-﻿using Avalonia.Controls;
+﻿using System;
+using System.Collections.Specialized;
+using Avalonia.Controls;
 using Avalonia.Diagnostics.ViewModels;
 using Avalonia.Input;
+using Avalonia.LogicalTree;
 using Avalonia.Markup.Xaml;
+using Avalonia.Threading;
 
 namespace Avalonia.Diagnostics.Views
 {
     internal class ConsoleView : UserControl
     {
+        private readonly ListBox _historyList;
         private readonly TextBox _input;
 
         public ConsoleView()
         {
             this.InitializeComponent();
+            _historyList = this.FindControl<ListBox>("historyList");
+            ((ILogical)_historyList).LogicalChildren.CollectionChanged += HistoryChanged;
             _input = this.FindControl<TextBox>("input");
             _input.KeyDown += InputKeyDown;
         }
@@ -21,6 +28,14 @@ namespace Avalonia.Diagnostics.Views
         private void InitializeComponent()
         {
             AvaloniaXamlLoader.Load(this);
+        }
+
+        private void HistoryChanged(object sender, NotifyCollectionChangedEventArgs e)
+        {
+            if (e.Action == NotifyCollectionChangedAction.Add && e.NewItems[0] is IControl control)
+            {
+                DispatcherTimer.RunOnce(control.BringIntoView, TimeSpan.Zero);
+            }
         }
 
         private void InputKeyDown(object sender, KeyEventArgs e)
