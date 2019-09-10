87f12673fe0af4738fbe9df565c761353410c7e4
Steven Kirk <grokys@gmail.com>
2019-09-10 16:09:49 +0200

    Enable ctrl+shift selection.

--- src/Avalonia.Diagnostics/Diagnostics/Views/MainWindow.xaml.cs
+++ src/Avalonia.Diagnostics/Diagnostics/Views/MainWindow.xaml.cs
@@ -1,17 +1,29 @@
-﻿using Avalonia.Controls;
+﻿using System;
+using System.Linq;
+using System.Reactive.Linq;
+using Avalonia.Controls;
+using Avalonia.Controls.Primitives;
 using Avalonia.Diagnostics.ViewModels;
+using Avalonia.Input;
+using Avalonia.Input.Raw;
 using Avalonia.Markup.Xaml;
 using Avalonia.Styling;
+using Avalonia.VisualTree;
 
 namespace Avalonia.Diagnostics.Views
 {
     internal class MainWindow : Window, IStyleHost
     {
         private TopLevel _root;
+        private IDisposable _keySubscription;
 
         public MainWindow()
         {
             InitializeComponent();
+
+            _keySubscription = InputManager.Instance.Process
+                .OfType<RawKeyEventArgs>()
+                .Subscribe(RawKeyDown);
         }
 
         public TopLevel Root
@@ -29,9 +41,33 @@ namespace Avalonia.Diagnostics.Views
 
         IStyleHost IStyleHost.StylingParent => null;
 
+        protected override void HandleClosed()
+        {
+            base.HandleClosed();
+            _keySubscription.Dispose();
+        }
+
         private void InitializeComponent()
         {
             AvaloniaXamlLoader.Load(this);
+        }
+
+        private void RawKeyDown(RawKeyEventArgs e)
+        {
+            const RawInputModifiers modifiers = RawInputModifiers.Control | RawInputModifiers.Shift;
+
+            if (e.Modifiers == modifiers)
+            {
+                var point = (Root as IInputRoot)?.MouseDevice?.GetPosition(Root) ?? default;
+                var control = Root.GetVisualsAt(point, x => (!(x is AdornerLayer) && x.IsVisible))
+                    .FirstOrDefault();
+
+                if (control != null)
+                {
+                    var vm = (MainViewModel)DataContext;
+                    vm.SelectControl((IControl)control);
+                }
+            }
         }
     }
 }
